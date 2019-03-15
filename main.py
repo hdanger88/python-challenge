2b95562e06f9a41636c8b24d7d84291bf712c7d9
hdanger88 <47699120+hdanger88@users.noreply.github.com>
2019-03-14 23:40:30 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+# The net total amount of "Profit/Losses" over the entire period
+# The average of the changes in "Profit/Losses" over the entire period
+# The greatest increase in profits (date and amount) over the entire period
+# The greatest decrease in losses (date and amount) over the entire period
+
+#import modules
+import os
+import csv
+
+#set variables
+csvpath = budget_data.csv
+total_months =
+net_total =
+average_change =
+greatest_increase =
+greatest_decrease =
+
+#read csv
+with open(csvpath, newline='') as handler:
+	budget_data = csv.reader(handler)
+	header = next(budget_data) 
+
+	
+
+
+
+
+
+#print outputs
+
+
