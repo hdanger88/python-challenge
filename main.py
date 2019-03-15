732cc82dbf35676448bc3f8d40077a06f1b49601
hdanger88 <47699120+hdanger88@users.noreply.github.com>
2019-03-14 23:58:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,11 @@ import csv
 #set variables
 csvpath = budget_data.csv
 total_months =
+current_month
+last_month
 net_total =
 average_change =
+daily_change = 
 greatest_increase =
 greatest_decrease =
 
@@ -20,12 +23,35 @@ with open(csvpath, newline='') as handler:
 	budget_data = csv.reader(handler)
 	header = next(budget_data) 
 
+#loop through data and count months, don't count header
+#loop through and sum colum of profit and loss
+	for row in budget_data
 	
 
+#calculate average profit and loss
 
 
+#loop through calculate daily change, store daily change, identiy max and min
 
 
 #print outputs
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months:
+print(f"Total: $
+print(f"Average Change: $
+print(f"Greatest Increase in Profits:
+print(f"Greatest Decrease in Profits:
 
+#write to txt file
+output_path = 
+
+with open(, 'w') as handler:
+	handler.write("Financial Analysis")
+	handler.write("----------------------------")
+	handler.write(f"Total Months:")
+	handler.write(f"Total: $")
+	handler.write(f"Average Change: $")
+	handler.write(f"Greatest Increase in Profits: ")
+	handler.write(f"Greatest Decrease in Profits: ")
 
